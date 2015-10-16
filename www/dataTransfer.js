ba2f958dcb56c5773794b47ec7c8ea09fd90402d
Casper <bilei905060@icloud.com>
2015-10-16 13:18:39 +0800

    update plugin call method

--- www/dataTransfer.js
+++ www/dataTransfer.js
@@ -1,10 +1,15 @@
 module.exports = {
     upload: function(postURL, postData, successCallback, errorCallback) {
-        var photoURL = postData.photoURL;
+        var photoURL;
+        var voiceURL;
+        
+        if (postData.photoURL != undefined) photoURL = postData.photoURL;
+        if (postData.voiceURL != undefined) voiceURL = postData.voiceURL;
+        
         cordova.exec(successCallback,
                      errorCallback,
                      "dataTransfer",
                      "upload",
-                     [postURL, postData, photoURL]);
+                     [postURL, postData, photoURL, voiceURL]);
     }
 };
