fd75043f7f70b64c94a79d7fa83da885c50759be
Casper <bilei905060@icloud.com>
2015-11-05 11:18:31 +0800

    update plugin.xml

--- www/dataTransceiver.js
+++ www/dataTransceiver.js
@@ -3,11 +3,11 @@ module.exports = {
         var photoURL = new Array();
         var voiceURL = new Array();
         
-        if (postData.photoURL != undefined) {
+        if ((postData.photoURL != undefined) || postData.voiceURL) {
             photoURL = postData.photoURL;
             postData.photoURL = undefined;
         }
-        if (postData.voiceURL != undefined) {
+        if ((postData.voiceURL != undefined) || postData.voiceURL) {
             voiceURL = postData.voiceURL;
             postData.voiceURL = undefined;
         }
